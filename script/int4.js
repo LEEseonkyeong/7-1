90dad3b33e295e6f6fd778fa3a9dcee8385fb363
LEEseonkyeong <o0osk@kakao.com>
2024-07-01 14:12:35 +0900

    1st commit

---
+++ script/int4.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(".btn1").click(function(){
+        $("body").animate({"background-color" : "red"}, 1000)
+    })  //btn1 click
+
+
+    $(".btn2").click(function(){
+        $("body").animate({"background-color" : "yellow"}, 1000);
+    }) //btn2 click
+
+
+    $(".btn3").click(function(){
+        $("body").animate({"background-color" : "green"}, 1000)
+    }) //btn3 click
+
+
+    $(".btn4").click(function(){
+        $("body").animate({"background-color" : "blue"}, 1000)
+    }) //btn4 click
+})
---
+++ script/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".btn").click(function () {
+        // $(".img_box").css("width", "100%");
+        // $(".img_box").css({"width": "100%"});
+
+        $(".img_box").animate({"width": "100%", "opacity" : "1"}, 1000)
+    }) //btn click
+
+}) //jQuery end
